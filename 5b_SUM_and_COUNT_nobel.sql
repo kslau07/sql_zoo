048fdd5b57890d6704527ff95bf4d11aef935d98
sam <abluecar@gmail.com>
2023-02-20 00:18:41 -0500

    add answers for exercises 1-5

--- 5b_SUM_and_COUNT_nobel.sql
+++ 5b_SUM_and_COUNT_nobel.sql
@@ -2,7 +2,8 @@
 
 -- 1. Show the total number of prizes awarded.
 
-SELECT COUNT(winner) FROM nobel
+SELECT COUNT(winner)
+FROM nobel
 
 -- 2. List each subject - just once
 
@@ -19,33 +20,37 @@ HAVING subject = 'Physics';
 
 -- 4. For each subject show the subject and the number of prizes.
 
-SELECT subject, COUNT(subject)
+SELECT subject,
+       COUNT(subject)
 FROM nobel
 GROUP BY subject;
 
 -- 5. For each subject show the first year that the prize was awarded.
 
-SELECT subject, MIN(yr)
+SELECT subject,
+       MIN(yr)
 FROM nobel
 GROUP BY subject;
 
-
 -- 6. For each subject show the number of prizes awarded in the year 2000.
 
-SELECT subject, COUNT(yr)
+SELECT subject,
+       COUNT(yr)
 FROM nobel
 WHERE yr = 2000
 GROUP BY subject;
 
 -- 7. Show the number of different winners for each subject.
 
-SELECT subject, COUNT(DISTINCT winner)
+SELECT subject,
+       COUNT(DISTINCT winner)
 FROM nobel
 GROUP BY subject;
 
 -- 8. For each subject show how many years have had prizes awarded.
 
-SELECT subject, COUNT(DISTINCT yr)
+SELECT subject,
+       COUNT(DISTINCT yr)
 FROM nobel
 GROUP BY subject;
 
@@ -76,8 +81,10 @@ AND COUNT(DISTINCT subject) > 1;
 
 -- 12. Show the year and subject where 3 prizes were given. Show only years 2000 onwards.
 
-SELECT yr, subject
+SELECT yr,
+       subject
 FROM nobel
 WHERE yr >= 2000
-GROUP BY yr, subject
+GROUP BY yr,
+         subject
 HAVING COUNT(winner) = 3;
